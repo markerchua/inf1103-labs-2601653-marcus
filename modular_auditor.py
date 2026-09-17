954b5c9cadb642a11f4aa8045ba8a370d78def65
Marcus <marcusccl16@gmail.com>
2026-09-17 23:51:03 +0800

    Run in a continuous loop until quit

--- modular_auditor.py
+++ modular_auditor.py
@@ -8,3 +8,12 @@ print("=======================")
 inventory = 0
 rejected = 0
 
+while (True):
+    
+    quantity =input("Please enter a stock quantity or type 'quit' to get your final report: ")
+
+    if(quantity.lower() == "quit"):
+            print("Final Report:")
+            print("Total Units Processed: "+str(inventory))
+            print("Total Rejected/Failed Entries: "+str(rejected))
+            break
